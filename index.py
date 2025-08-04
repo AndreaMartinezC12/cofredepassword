032077834ac09e43fac525107244a534f9220fe3
AndreaMartinezC <andrea12_94@hotmail.com>
2025-08-03 22:20:41 -0600

    Ya se guardan los valores ingresados en "usuario" y "password" en un csv

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from tkinter import ttk
+import pandas as pd
+import os
 
 
 class PasswordApp:
@@ -10,17 +12,28 @@ class PasswordApp:
         self.root.geometry("400x300")
         self.pdf_files=[]
 
+        #obtener la ruta del directorio donde esta este script
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+
+        #construir la ruta al archivo CSV
+        csv_path=os.path.join(script_dir,"data","passwords.csv")
+
+        self.df=pd.read_csv(csv_path)
+         
+
         tk.Label(root, text="Usuario: ").pack()
-        entrada_usuario = tk.Entry(root)
-        entrada_usuario.pack()
+        self.entrada_usuario = tk.Entry(root)
+        self.entrada_usuario.pack()
 
         tk.Label(root, text="Contrasena: ").pack()
-        entrada_contrasena = tk.Entry(root)
-        entrada_contrasena.pack()
+        self.entrada_contrasena = tk.Entry(root)
+        self.entrada_contrasena.pack()
 
         ttk.Button(root, text="Guardar cuenta", command=self.save_account).pack(pady=10)
         
     def save_account(self):
+        self.df.loc[len(self.df)] = [self.entrada_usuario.get(), self.entrada_contrasena.get()]
+        print(str(self.df) + "\n")
         print("ok")
   
 
