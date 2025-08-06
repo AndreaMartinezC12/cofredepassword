16273dacbf3ec83f98fb91c712ad908740807f2f
AndreaMartinezC <andrea12_94@hotmail.com>
2025-08-05 18:45:33 -0600

    Agregando funcion de hacer busquedas por nombre de aplicacion o nombre de usuario

--- index.py
+++ index.py
@@ -20,6 +20,9 @@ class PasswordApp:
 
         self.df=pd.read_csv(csv_path)
          
+        tk.Label(root, text="Aplicacion: ").pack()
+        self.entrada_aplicacion = tk.Entry(root)
+        self.entrada_aplicacion.pack()
 
         tk.Label(root, text="Usuario: ").pack()
         self.entrada_usuario = tk.Entry(root)
@@ -30,15 +33,50 @@ class PasswordApp:
         self.entrada_contrasena.pack()
 
         ttk.Button(root, text="Guardar cuenta", command=self.save_account).pack(pady=10)
+
+        tk.Label(root, text="Busqueda: ").pack()
+        self.entrada_busqueda = tk.Entry(root)
+        self.entrada_busqueda.pack()
+
+        tk.Label(root, text="Buscar por: ").pack()
+        # Create a StringVar to hold the combobox's value
+        selected_option = tk.StringVar()
+
+        # Create the Combobox
+        self.my_combobox = ttk.Combobox(root, textvariable=selected_option,
+                           values=("Aplicacion", "Usuario"))
+        self.my_combobox.set("Select") # Set initial display text
+
+        # Bind the <<ComboboxSelected>> event to a function
+        self.my_combobox.bind("<<ComboboxSelected>>", self.set_tipobusqueda)
+
+        self.my_combobox.pack(pady=10)
+
+        ttk.Button(root, text="Buscar", command=self.search_account).pack(pady=10)
         
     def save_account(self):
         password = self.entrada_contrasena.get().encode()
         hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-        self.df.loc[len(self.df)] = [self.entrada_usuario.get(), hashed]
+        self.df.loc[len(self.df)] = [self.entrada_aplicacion.get(), self.entrada_usuario.get(), hashed]
         print(str(self.df) + "\n")
-        print("ok")
-  
 
+    def set_tipobusqueda(self, event):
+        selected_value = self.my_combobox.get()
+        print(f"Selected: {selected_value}")
+
+    def search_account(self):
+        tipo_busqueda = self.my_combobox.get()
+        if tipo_busqueda == "Aplicacion":
+            result = self.df[self.df['Aplicacion'].str.contains(self.entrada_busqueda.get(), na=False)]
+            print(result)
+        elif tipo_busqueda == "Usuario":
+            result = self.df[self.df['Usuario'].str.contains(self.entrada_busqueda.get(), na=False)]
+            print("algo")
+        else:
+            print("algo")
+
+        tk.Label(root, text=result).pack()
+  
 
 if __name__ == "__main__":
     root =tk.Tk()
